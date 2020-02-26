d72224bffc43447a7713fa94bafe9f867acd1910
mprince2k18 <mprince2k16@gmail.com>
2020-02-26 15:46:46 +0600

    product bug

--- database/migrations/2019_12_10_094552_create_products_table.php
+++ database/migrations/2019_12_10_094552_create_products_table.php
@@ -17,12 +17,26 @@ class CreateProductsTable extends Migration
             $table->bigIncrements('id');
             $table->string('product_title');
             $table->longText('product_details');
+            $table->longText('product_details_ban');
             $table->string('product_photo');
+
+            $table->integer('product_id');
+            $table->string('title');
+            $table->longText('dis');
+            $table->string('titleBan');
+            $table->longText('disBan');
+
+            $table->string('title');
+            $table->longText('dis');
+            $table->string('titleBan');
+            $table->longText('disBan');
+
+
             $table->softDeletes();
             $table->timestamps();
         });
     }
-    
+
     /**
      * Reverse the migrations.
      *

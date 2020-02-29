8856611ae443af073d074ed86b2bd970fc8dd71e
mprince2k18 <mprince2k16@gmail.com>
2020-02-29 15:18:50 +0600

    jhfgjg

--- resources/views/frontend/management_committee/index.blade.php
+++ resources/views/frontend/management_committee/index.blade.php
@@ -99,15 +99,15 @@
 </section>
 
 
-<div id="board-mem" style="margin: 50px 0 0 0;">
+{{-- <div id="board-mem" style="margin: 50px 0 0 0;">
   <div class="container">
-    
+
       <div class="row">
         @foreach ($management_commitees as $management_commitee)
 
-          <div class="col-md-3">
+          <div class="col-md-3 aunModalFrom">
             <a href="#" data-toggle="modal" data-target=".bd-example-modal-xl">
-              <div class="icon-box icon-box-15 text-center far_board">
+              <div class="icon-box icon-box-15 text-center far_board aunModalFromDiv">
                   <img src="{{ asset('/uploads/management') }}/{{ $management_commitee->photo }}" class="img-fluid" style="width: 100%" alt="">
                   <h6>{!! html_entity_decode($management_commitee->name) !!}</h6>
                   <p class="text-dark">{!! html_entity_decode(Str::limit($management_commitee->speaks,10)) !!}</p>
@@ -118,38 +118,37 @@
 
 
 
-          <!-- Extra large modal START-->
-
-
-          <div class="modal fade bd-example-modal-xl" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-xl">
-              <div class="modal-content">
-                <div class="container">
-                  <div class="row">
-                    <div class="col-md-10 offset-md-1">
-                      <div class="far_modal p-5">
-                        <div class="far_img text-center">
-                          <img src="{{ asset('uploads/management') }}/{{ $management_commitee->photo }}" class="img-fluid" alt="">
-                        </div>
-                        <div class="far_para pt-2">
-                          <h2 class="text-center text-dark">{!! html_entity_decode($management_commitee->name) !!}</h2>
-                          <p class="text-justify text-dark">{!! html_entity_decode($management_commitee->speaks) !!}</p>
-                        </div>
-                      </div>
-
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <!-- Extra large modal END-->
 
         @endforeach
 
 
+        <!-- Extra large modal START-->
 
+
+                              <div class="modal fade bd-example-modal-xl" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+                                <div class="modal-dialog modal-xl">
+                                  <div class="modal-content">
+                                    <div class="container">
+                                      <div class="row">
+                                        <div class="col-md-10 offset-md-1">
+                                          <div class="far_modal p-5">
+                                            <div class="far_img text-center">
+                                              <img id="aunModalToImg" src="" class="img-fluid" alt="">
+                                            </div>
+                                            <div id="aunModalTo" class="far_para pt-2">
+                                              <h2 id="aunModalToh" class="text-center text-dark"></h2>
+                                              <p id="aunModalTod" class="text-justify text-dark"></p>
+                                            </div>
+                                          </div>
+
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+
+                              <!-- Extra large modal END-->
 
 
 
@@ -159,8 +158,69 @@
       </div>
 
   </div>
-</div>
+</div> --}}
 
+
+
+      <div id="board-mem">
+          <div class="container">
+              <div class="row">
+
+                @foreach ($management_commitees as $board_of_directors)
+
+                  <div class="col-md-3 aunModalFrom">
+                      <div class="aunModalFromDiv icon-box icon-box-15 text-center far_board">
+                        <a class="" href="#" data-toggle="modal" data-target=".bd-example-modal-xl">
+                          <img src="{{ asset('/uploads/management') }}/{{ $board_of_directors->photo }}" class="img-fluid imgaunfrom" style="width: 100%" alt="">
+                        </a>
+                          <h6>{!! html_entity_decode($board_of_directors->name) !!}</h6>
+                          <div style="display: none;">{!! html_entity_decode($board_of_directors->speaks) !!}</div>
+                          <p class="text-dark">{!! html_entity_decode(Str::limit($board_of_directors->speaks,10)) !!}</p>
+                      </div>
+
+                  </div>
+
+                @endforeach
+
+                <!-- Extra large modal START-->
+
+
+                <div class="modal fade bd-example-modal-xl" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+                  <div class="modal-dialog modal-xl">
+                    <div class="modal-content" style="border: 5px solid #000;">
+                      <div class="container">
+                        <div class="row">
+                          <div class="col-md-10 offset-md-1">
+                            <div class="far_modal p-5">
+                              <div class="far_img text-right">
+                                <img id="aunModalToImg" src="" class="img-fluid" alt="">
+                              </div>
+                              <div id="aunModalTo" class="far_para pt-2">
+                                <h2 id="aunModalToh" class="text-right text-dark"></h2>
+                                <p id="aunModalTod" class="text-justify text-dark"></p>
+                              </div>
+                            </div>
+
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <!-- Extra large modal END-->
+
+
+
+
+
+
+
+
+              </div>
+
+          </div>
+      </div>
 
 
 @endsection
@@ -168,43 +228,49 @@
 
 @section('js')
 
-{{-- <script>
+  <script>
 
-$('.committee').click(function(e){
-  // alert(e.target.dataset.id);
-  console.log();
-  if(e.target.dataset.id != undefined){
-    var makeUrl=window.location.origin+"/management-committee/"+e.target.dataset.id;
-    window.location.href=makeUrl;
+    $('.aunModalFrom').click((e)=>{
+      let target = e.target;
+      if (target.classList.contains('imgaunfrom')) {
+          let imageSrc = e.target.src;
+          let ph = target.parentElement.parentElement.childNodes[3].innerHTML;
+          let pd = target.parentElement.parentElement.childNodes[5].innerHTML;
+
+          document.getElementById('aunModalToImg').src = imageSrc;
+          document.getElementById('aunModalToh').innerHTML = ph;
+          document.getElementById('aunModalTod').innerHTML = pd;
+      }
+
+    });
+
+  </script>
+
+  <script>
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var img = document.getElementById("myImg");
+  var modalImg = document.getElementById("img01");
+  var captionText = document.getElementById("caption");
+  img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
   }
-});
 
-</script> --}}
-<script>
-// Get the modal
-var modal = document.getElementById("myModal");
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  </script>
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-</script>
-
-<script type="text/javascript">
-  $(".modal").each(function(l){$(this).on("show.bs.modal",function(l){var o=$(this).attr("data-easein");"shake"==o?$(".modal-dialog").velocity("callout."+o):"pulse"==o?$(".modal-dialog").velocity("callout."+o):"tada"==o?$(".modal-dialog").velocity("callout."+o):"flash"==o?$(".modal-dialog").velocity("callout."+o):"bounce"==o?$(".modal-dialog").velocity("callout."+o):"swing"==o?$(".modal-dialog").velocity("callout."+o):$(".modal-dialog").velocity("transition."+o)})});
-</script>
+  <script type="text/javascript">
+    $(".modal").each(function(l){$(this).on("show.bs.modal",function(l){var o=$(this).attr("data-easein");"shake"==o?$(".modal-dialog").velocity("callout."+o):"pulse"==o?$(".modal-dialog").velocity("callout."+o):"tada"==o?$(".modal-dialog").velocity("callout."+o):"flash"==o?$(".modal-dialog").velocity("callout."+o):"bounce"==o?$(".modal-dialog").velocity("callout."+o):"swing"==o?$(".modal-dialog").velocity("callout."+o):$(".modal-dialog").velocity("transition."+o)})});
+  </script>
 
 @endsection

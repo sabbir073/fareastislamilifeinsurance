5e15e45b41bceb17462516b31129e471e37ad2eb
mprince2k18 <mprince2k16@gmail.com>
2020-03-04 01:01:05 +0600

    sadasd

--- resources/views/frontend/homepage/app.blade.php
+++ resources/views/frontend/homepage/app.blade.php
@@ -715,187 +715,6 @@ padding-bottom: 25px;
 }
 /* premium_calculator */
 
-
-
-.menu {
-  margin: 15px;
-}
-.menu > ol {
-  list-style: none;
-  margin: 30px 0;
-  padding: 0;
-}
-.menu > ol > li {
-  background: #3c3c3c;
-  border-left: 5px solid var(--gradient-color);
-  margin-bottom: 1px;
-  position: relative;
-  transition: 0.5s;
-}
-.menu > ol > li:nth-child(1) {
-  --accent-color: #FDA085;
-  --gradient-color: #F6D365;
-}
-.menu > ol > li:nth-child(2) {
-  --accent-color: #BFF098;
-  --gradient-color: #6FD6FF;
-}
-.menu > ol > li:nth-child(3) {
-  --accent-color: #EA8D8D;
-  --gradient-color: #A890FE;
-}
-.menu > ol > li:nth-child(4) {
-  --accent-color: #D8B5FF;
-  --gradient-color: #1EAE98;
-}
-.menu > ol > li:nth-child(5) {
-  --accent-color: #C6EA8D;
-  --gradient-color: #FE90AF;
-}
-.menu > ol > li a {
-  color: #FBFBFB;
-  display: block;
-  padding: 15px;
-  position: relative;
-  text-decoration: none;
-  z-index: 1;
-}
-.menu > ol > li a:not(:last-child):before {
-  content: "\f078";
-  font-family: fontAwesome;
-  font-size: 0.75em;
-  line-height: 50px;
-  position: absolute;
-  right: 25px;
-  top: 0;
-  bottom: 0;
-  margin: auto;
-  transition: 0.5s;
-}
-.menu > ol > li:focus,
-.menu > ol > li:focus-within,
-.menu > ol > li:hover {
-  z-index: 100;
-}
-.menu > ol > li:focus:after,
-.menu > ol > li:focus-within:after,
-.menu > ol > li:hover:after {
-  background: linear-gradient(to left, var(--accent-color), var(--gradient-color));
-  max-width: 800px;
-}
-.menu > ol > li:focus .sub-menu,
-.menu > ol > li:focus-within .sub-menu {
-  max-height: 500px;
-}
-.menu > ol > li:focus a:before,
-.menu > ol > li:focus-within a:before {
-  transform: rotate(-180deg);
-}
-.menu > ol > li:after {
-  background: #3c3c3c;
-  content: "";
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 0;
-  bottom: 0;
-  transition: 0.5s;
-  max-width: 0;
-  overflow: hidden;
-}
-.menu > ol .sub-menu {
-  border-left: 1px solid #FBFBFB;
-  margin-left: 22.5px;
-  list-style: none;
-  max-height: 0px;
-  overflow: hidden;
-  padding-left: 7.5px;
-  position: relative;
-  transition: 0.5s;
-  z-index: 1;
-}
-.menu > ol .sub-menu li {
-  font-size: 0.9em;
-}
-.menu > ol .sub-menu li:hover,
-.menu > ol .sub-menu li a:focus {
-  background: rgba(60, 60, 60, 0.3);
-}
-.menu > ol .sub-menu li a:after {
-  bottom: 5px;
-  height: 1px;
-}
-.menu > ol .sub-menu li a:hover:after,
-.menu > ol .sub-menu li a:focus:after {
-  width: 15px;
-}
-@media (min-width: 600px) {
-  a:focus,
-  a:hover {
-    position: relative;
-  }
-  a:focus:after,
-  a:hover:after {
-    width: 50px;
-  }
-  a:after {
-    left: 0;
-    right: 0;
-    margin: auto;
-  }
-  .menu {
-    margin: 0;
-    /* margin-top: 40vh; */
-  }
-  .menu > ol {
-    display: block;
-    max-width: none;
-    text-align: center;
-  }
-  .menu > ol > li {
-    border-top: 5px solid var(--accent-color);
-    border-left: 0;
-    display: inline-block;
-    margin-left: -5px;
-    vertical-align: top;
-    width: 120px;
-  }
-  .menu > ol > li:hover:after,
-  .menu > ol > li:focus:after,
-  .menu > ol > li:focus-within:after {
-    background: linear-gradient(to bottom, var(--accent-color), var(--gradient-color));
-    border-radius: 3px;
-    top: -15px;
-    bottom: -15px;
-    left: -15px;
-    right: -15px;
-  }
-  .menu > ol > li:hover .sub-menu,
-  .menu > ol > li:focus .sub-menu,
-  .menu > ol > li:focus-within .sub-menu {
-    max-height: 750px;
-  }
-  .menu > ol > li a:not(:last-child):before {
-    right: 12.5px;
-  }
-  .menu > ol > li:hover a:before {
-    transform: rotate(-180deg);
-  }
-  .menu > ol .sub-menu {
-    border-left: 0;
-    margin: 15px -15px -15px;
-    padding-left: 0;
-  }
-}
-@media (min-width: 775px) {
-  .menu > ol > li {
-    width: 150px;
-  }
-  .menu > ol > li a:not(:last-child):before {
-    right: 25px;
-  }
-}
-
 @yield('css')
 
 {{-- --------------------------- --}}
@@ -968,6 +787,202 @@ padding-top: 50px;
 
 
 {{-- ------------------- --}}
+
+.menu {
+  /* background: #F67280; */
+  height: 4rem;
+  width: 100%;
+}
+.menu ol {
+  list-style-type: none;
+  margin: 0 auto;
+  padding: 0;
+}
+.menu > ol {
+  max-width: 1000px;
+  padding: 0 2rem;
+  display: flex;
+}
+.menu > ol > .menu-item {
+  flex: 1;
+  padding: 0.75rem 0;
+}
+.menu > ol > .menu-item:after {
+  content: "";
+  position: absolute;
+  width: 4px;
+  height: 4px;
+  border-radius: 50%;
+  bottom: 5px;
+  left: calc(50% - 2px);
+  background: #FECEAB;
+  will-change: transform;
+  transform: scale(0);
+  transition: transform 0.2s ease;
+}
+.menu > ol > .menu-item:hover:after {
+  transform: scale(1);
+}
+.menu-item {
+  position: relative;
+  line-height: 2.5rem;
+  text-align: center;
+}
+.menu-item a {
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  display: block;
+  color: #FFF;
+}
+.sub-menu .menu-item {
+  padding: 0.75rem 0;
+  background: #F67280;
+  opacity: 0;
+  transform-origin: bottom;
+  animation: enter 0.2s ease forwards;
+}
+.sub-menu .menu-item:nth-child(1) {
+  animation-duration: 0.2s;
+  animation-delay: 0s;
+}
+.sub-menu .menu-item:nth-child(2) {
+  animation-duration: 0.3s;
+  animation-delay: 0.1s;
+}
+.sub-menu .menu-item:nth-child(3) {
+  animation-duration: 0.4s;
+  animation-delay: 0.2s;
+}
+.sub-menu .menu-item:hover {
+  background: #F8B195;
+}
+.sub-menu .menu-item a {
+  padding: 0 0.75rem;
+}
+@media screen and (max-width: 600px) {
+  .sub-menu .menu-item {
+    background: #C06C84;
+  }
+}
+@media screen and (max-width: 600px) {
+  .menu {
+    position: relative;
+  }
+  .menu:after {
+    content: "";
+    position: absolute;
+    top: calc(50% - 2px);
+    right: 1rem;
+    width: 30px;
+    height: 4px;
+    background: #FFF;
+    box-shadow: 0 10px #FFF, 0 -10px #FFF;
+  }
+  .menu > ol {
+    display: none;
+    background: #F67280;
+    flex-direction: column;
+    justify-content: center;
+    height: 100vh;
+    animation: fade 0.2s ease-out;
+  }
+  .menu > ol > .menu-item {
+    flex: 0;
+    opacity: 0;
+    animation: enter 0.3s ease-out forwards;
+  }
+  .menu > ol > .menu-item:nth-child(1) {
+    animation-delay: 0s;
+  }
+  .menu > ol > .menu-item:nth-child(2) {
+    animation-delay: 0.1s;
+  }
+  .menu > ol > .menu-item:nth-child(3) {
+    animation-delay: 0.2s;
+  }
+  .menu > ol > .menu-item:nth-child(4) {
+    animation-delay: 0.3s;
+  }
+  .menu > ol > .menu-item:nth-child(5) {
+    animation-delay: 0.4s;
+  }
+  .menu > ol > .menu-item + .menu-item {
+    margin-top: 0.75rem;
+  }
+  .menu > ol > .menu-item:after {
+    left: auto;
+    right: 1rem;
+    bottom: calc(50% - 2px);
+  }
+  .menu > ol > .menu-item:hover {
+    z-index: 1;
+  }
+  .menu:hover > ol {
+    display: flex;
+  }
+  .menu:hover:after {
+    box-shadow: none;
+  }
+}
+
+.sub-menu {
+  position: absolute;
+  width: 100%;
+  top: 100%;
+  left: 0;
+  display: none;
+  z-index: 1;
+}
+.menu-item:hover > .sub-menu {
+  display: block;
+}
+@media screen and (max-width: 600px) {
+  .sub-menu {
+    width: 100vw;
+    left: -2rem;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+}
+
+html, body {
+  font-size: 16px;
+  font-family: "Fira Mono", monospace;
+  margin: 0;
+  background: #2A363B;
+}
+
+* {
+  box-sizing: border-box;
+}
+*:before, *:after {
+  box-sizing: inherit;
+}
+
+a {
+  text-decoration: none;
+}
+
+@keyframes enter {
+  from {
+    opacity: 0;
+    transform: scaleY(0.98) translateY(10px);
+  }
+  to {
+    opacity: 1;
+    transform: none;
+  }
+}
+@keyframes fade {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
 </style>
 
 
@@ -1142,7 +1157,7 @@ padding-top: 50px;
 
                     <img class="img-fluid hidden" src="https://res.cloudinary.com/onexa/image/upload/v1582544620/Final_1_oulatn.png" width="180px" height="150px" style="display:inline; margin: 12px 0px 0px 107px;">
 
-                        <div class="navbar navbar-expand-lg navbar-dark far_nav pull-right">
+                        <div class="navbar navbar-expand-lg navbar-dark far_nav pull-right" style="width: 75%;">
                             <a href="#" class="navbar-brand"></a>
                             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
                                 <span class="navbar-toggler-icon"></span>
@@ -1199,7 +1214,6 @@ padding-top: 50px;
                                      </li>
 
                                 </ul> --}}
-
 
                                 <nav class="menu">
   <ol>

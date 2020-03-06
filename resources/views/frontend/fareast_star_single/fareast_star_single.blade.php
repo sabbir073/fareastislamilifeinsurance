b62300c269985a3a98d829751625df48bcb527d1
mprince2k18 <mprince2k16@gmail.com>
2020-03-07 01:03:30 +0600

    ada

--- resources/views/frontend/fareast_star_single/fareast_star_single.blade.php
+++ resources/views/frontend/fareast_star_single/fareast_star_single.blade.php
@@ -29,7 +29,7 @@
 
 
 
-<section class="blog-single-wrapper section-padding">
+{{-- <section class="blog-single-wrapper section-padding">
         <div class="container">
             <div class="row">
                 <div class="col-lg-10 offset-lg-1">
@@ -38,27 +38,78 @@
                             <img src="{{ asset('uploads/fareaststar') }}/{{ $fareast_star_single->thumbnail }}" style="width:20% !important" class="img-fluid" alt="">
                         </div>
                         <div class="post-content">
-
                             <div class="post-header">
                                 <h3>{{ $fareast_star_single->name }}</h3>
 
                             </div>
-
-
                             <div class="post-body">
                               {!! html_entity_decode($fareast_star_single->far_star_desc) !!}
                             </div>
-
-
-
                         </div>
-                    </div><!-- ends: .post-details -->
-
-
-
-                </div><!-- ends: .col-lg-8 -->
+                    </div>
+                </div>
             </div>
         </div>
+    </section> --}}
+
+
+    <section class="section-bg p-top-60 p-bottom-60">
+
+    <div class="tab tab--1 section-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="tab_nav cardify">
+                        <div class="nav flex-column" id="tab_nav1" role="tablist" aria-orientation="vertical">
+                            <a class="nav-link active" id="v-pills-tab1" data-toggle="pill" href="#tab1_content" role="tab" aria-controls="tab1_content" aria-selected="true">2015</a>
+                            <a class="nav-link" id="v-pills-tab2" data-toggle="pill" href="#tab2_content" role="tab" aria-controls="tab2_content" aria-selected="false">2016</a>
+                            <a class="nav-link" id="v-pills-tab3" data-toggle="pill" href="#tab3_content" role="tab" aria-controls="tab3_content" aria-selected="false">2017</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-8">
+                    <div class="tab-content" id="tabContent1">
+                        <div class="tab-pane animated fadeIn show active" id="tab1_content" role="tabpanel" aria-labelledby="tab1_content">
+                            <figure>
+                                <img src="img/tabimg2.jpg" alt="Images the foolda">
+                                <figcaption>
+                                    <h5>Matador Managed Private Cloud in Five Months</h5>
+                                    <p>Investig ationes demons trave runt lectores legere liushgfy quod legunt saeph
+                                        claritas nvestig ationes demons trave rungt. Investiga tiones demons averu.</p>
+                                    <a href="#" class="btn btn-primary">read more</a>
+                                </figcaption>
+                            </figure>
+                        </div>
+                        <div class="tab-pane animated fadeIn" id="tab2_content" role="tabpanel" aria-labelledby="tab2_content">
+                            <figure>
+                                <img src="img/tabimg.jpg" alt="Images the foolda">
+                                <figcaption>
+                                    <h5>Matador Managed Private Cloud in Five Months</h5>
+                                    <p>Investig ationes demons trave runt lectores legere liushgfy quod legunt saeph
+                                        claritas nvestig ationes demons trave rungt. Investiga tiones demons averu.</p>
+                                    <a href="#" class="btn btn-primary">read more</a>
+                                </figcaption>
+                            </figure>
+                        </div>
+                        <div class="tab-pane animated fadeIn" id="tab3_content" role="tabpanel" aria-labelledby="tab3_content">
+                            <figure>
+                                <img src="img/tabimg2.jpg" alt="Images the foolda">
+                                <figcaption>
+                                    <h5>Matador Managed Private Cloud in Five Months</h5>
+                                    <p>Investig ationes demons trave runt lectores legere liushgfy quod legunt saeph
+                                        claritas nvestig ationes demons trave rungt. Investiga tiones demons averu.</p>
+                                    <a href="#" class="btn btn-primary">read more</a>
+                                </figcaption>
+                            </figure>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><!-- ends .tab -->
+
     </section>
 
 
